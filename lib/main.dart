8921d9da63f72e10e02e7bb471eaa60ffce2a4e4
halodewakoding <halodewakoding@gmail.com>
2022-09-22 21:36:53 +0700

    first init

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'dart:developer';
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
+import 'package:scanner/scan-page.dart';
+
+void main() => runApp(const MaterialApp(home: MyHome()));
+
+class MyHome extends StatelessWidget {
+  const MyHome({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Flutter Demo Home Page')),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).push(MaterialPageRoute(
+              builder: (context) => const ScanPage(),
+            ));
+          },
+          child: const Text('Mulai SCAN'),
+        ),
+      ),
+    );
+  }
+}
